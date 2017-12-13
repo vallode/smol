64b8fe6398fafc832fae37882d3bff08a3327110
Mariusz Zarzycki <vallode@hotmail.co.uk>
2017-12-13 03:30:01 +0000

    Thinned down db

--- Smol/Smol.py
+++ Smol/Smol.py
@@ -6,25 +6,25 @@ from flask import Flask, request, session, g, redirect, \
     url_for, abort, \
     render_template, flash
 
+
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'smol.db'),
-))
 app.config.from_envvar('SMOL_SETTINGS', silent=True)
+
+DATABASE = os.path.join(app.root_path, 'smol.db')
 
 
 def get_db():
     if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
+        g.sqlite_db = sqlite3.connect(DATABASE)
+        g.sqlite_db.row_factory = sqlite3.Row
     return g.sqlite_db
 
 
-def connect_db():
-    db = sqlite3.connect(app.config['DATABASE'])
-    db.row_factory = sqlite3.Row
-    return db
+@app.teardown_appcontext
+def close_db(exception):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 
 def init_db():
@@ -32,6 +32,12 @@ def init_db():
     with app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
     db.commit()
+
+
+@app.cli.command('initdb')
+def init_db_command():
+    init_db()
+    print('Initialized the database.')
 
 
 def encode(u):
@@ -42,18 +48,6 @@ def decode(u):
     return str(base64.b64decode(bytes(str(u), 'UTF-8')), 'UTF-8')
 
 
-@app.cli.command('initdb')
-def initdb_command():
-    init_db()
-    print('SMOL initialized the database.')
-
-
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-
-
 @app.route('/')
 def index():
     return render_template('base.html')
@@ -62,22 +56,14 @@ def index():
 @app.route('/shorten', methods=['POST', 'GET'])
 def shorten_link():
     if request.method == 'GET':
-        return index()
+        return render_template('base.html')
 
     if not validators.url(request.form['original']):
         return render_template('base.html', error='Invalid link, please provide a valid URL')
 
-    url = encode(request.form['original'])
     db = get_db()
-
-    db.execute('INSERT INTO links (originalURL) VALUES (?)', [url])
-    db.commit()
-    cur = db.execute('SELECT last_insert_rowid()')
-
-    cur_id = (cur.fetchone()[0])
-    id_encoded = encode(cur_id)
-
-    db.execute('UPDATE links SET encodedURL = (?) WHERE id = (?)', [id_encoded, cur_id])
+    cur = db.execute('INSERT INTO links (originalURL) VALUES (?)', [encode(request.form['original'])])
+    id_encoded = encode(cur.lastrowid)
 
     return render_template('base.html', link=id_encoded)
 
@@ -85,10 +71,8 @@ def shorten_link():
 @app.route('/<link>', methods=['POST', 'GET'])
 def reroute(link):
     db = get_db()
-    link_id = decode(link)
+    cur = db.execute('SELECT originalURL FROM links WHERE id = (?)', [decode(link)])
 
-    cur = db.execute('SELECT originalURL FROM links WHERE id = (?)', [link_id])
+    link = decode(cur.fetchone()[0])
 
-    URL = decode(cur.fetchone()[0])
-
-    return redirect(URL)
+    return redirect(link)
--- Smol/schema.sql
+++ Smol/schema.sql
@@ -1,6 +1,5 @@
 DROP TABLE IF EXISTS links;
 CREATE TABLE links (
   id integer primary key autoincrement,
-  'originalURL' text,
-  'encodedURL' text
+  'originalURL' text
 );
